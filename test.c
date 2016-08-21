f1cf8a0c90e48b824a22183245ab044cd0bd6eab
chen <cnchenzw@outlook.com>
2016-08-21 20:26:21 +0800

    modify test.c

--- test.c
+++ test.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
-	char *s = "Hello, WORLD";
-	int i = 0;
-	
-//	textmode(0xE0);
-	printf("%s\n", s);
-	
-	for (; i < strlen(s); i++) {
-		putchar(tolower(s[i]));
-	}
+	printf("Hello world!\n");
 
 	return 0;
 }
